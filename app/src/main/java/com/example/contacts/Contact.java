2cf865f7cb3a3bee8769553a38756b6a63a94907
t_hrithik <hrithik.ranjan@lavainternational.in>
2025-03-05 15:02:46 +0530

    Contact list modified

--- app/src/main/java/com/example/contacts/Contact.java
+++ app/src/main/java/com/example/contacts/Contact.java
@@ -11,7 +11,7 @@ public class Contact {
     private String email;
     private String name;
 
-    public Contact( String email, String name) {
+    public Contact( String name, String email) {
         this.email = email;
         this.name = name;
     }
